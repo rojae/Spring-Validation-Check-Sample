28e860b401fba491c9074524a360a9763b30644c
rojae <rojae@kakao.com>
2022-10-30 21:42:29 +0900

    fix. api Response DataType

--- src/main/java/kr/rojae/valid/ValidController.java
+++ src/main/java/kr/rojae/valid/ValidController.java
@@ -1,6 +1,8 @@
 package kr.rojae.valid;
 
+import kr.rojae.valid.dto.ApiBase;
 import kr.rojae.valid.dto.MovieAddRequest;
+import kr.rojae.valid.dto.MovieAddResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +13,10 @@ import javax.validation.Valid;
 public class ValidController {
 
     @PostMapping("/")
-    public ResponseEntity<String> addMovie(@RequestBody @Valid MovieAddRequest request){
-        return ResponseEntity.status(HttpStatus.CREATED).body(request.getName());
+    public ResponseEntity<ApiBase> addMovie(@RequestBody @Valid MovieAddRequest request){
+        MovieAddResponse response = new MovieAddResponse();
+        response.setName(request.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiBase.builder().message("").response(response).build());
     }
 
 }
---
+++ src/main/java/kr/rojae/valid/dto/MovieAddResponse.java
@@ -0,0 +1,8 @@
+package kr.rojae.valid.dto;
+
+import lombok.Data;
+
+@Data
+public class MovieAddResponse {
+    private String name;
+}
