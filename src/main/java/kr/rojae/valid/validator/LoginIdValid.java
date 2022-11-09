95c6c0eff38e0c918749236954fe3add71cb66f5
rojae <rojae@kakao.com>
2022-11-09 22:22:28 +0900

    fix. LoginIdValid

--- src/main/java/kr/rojae/valid/validator/LoginIdValid.java
+++ src/main/java/kr/rojae/valid/validator/LoginIdValid.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Length(min = 5, max = 15, message = "loginId는 8자 이상, 15자 이하로 가능합니다.")
+@Length(min = 8, max = 15, message = "loginId는 8자 이상, 15자 이하로 가능합니다.")
 @NotBlank(message = "loginId는 빈 공간을 가질 수 없습니다.")
 @OnlyDigitWithAlphabetValid(message = "loginId는 숫자와 영어로만 이루어집니다.")
 @Constraint(validatedBy = {})
