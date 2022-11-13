f350fbf067ef5f2a8861aa5c0de93d7d1f05ab4d
rojae <rojae@kakao.com>
2022-11-13 20:11:16 +0900

    fix. MovieAddRequest

--- src/main/java/kr/rojae/valid/dto/MovieAddRequest.java
+++ src/main/java/kr/rojae/valid/dto/MovieAddRequest.java
@@ -2,18 +2,20 @@ package kr.rojae.valid.dto;
 
 import kr.rojae.valid.validator.MovieCategoryValid;
 import kr.rojae.valid.validator.common.extension.StringValid;
+import kr.rojae.valid.validator.common.format.IsYnValid;
+import kr.rojae.valid.validator.common.match.OnlyKoreanValid;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 public class MovieAddRequest {
-    @NotBlank(message = "MovieAddRequest.name은 빈 값일 수 없습니다")
+    @OnlyKoreanValid(message = "MovieAddRequest.name은 한글이여야 합니다")
     private String name;
 
     @MovieCategoryValid(message = "MovieAddRequest.category이 유효한 값이 아닙니다")
     private String category;
 
-    @StringValid(acceptedList = {"Y", "N"}, message = "MovieAddRequest.useYn이 유효한 값이 아닙니다")
+    @IsYnValid(message = "MovieAddRequest.useYn이 유효한 값이 아닙니다 (Y, N)")
     private String useYn;
 }
